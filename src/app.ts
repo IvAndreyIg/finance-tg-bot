9de12680b3e7a3392b8b01fdccb463ebeb684acc
bang <frostpunchofpain@gmail.com>
2022-02-10 13:48:44 +0300

    new changes

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
+require('dotenv').config();
+
+
+import {Telegraf} from 'telegraf'
+
+console.log('process.env.BOT_TOKEN', process.env.BOT_TOKEN)
+
+const bot = new Telegraf(<string>process.env.BOT_TOKEN)
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker or any any 3'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there!!'))
+bot.launch()
 console.info("hello");
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
